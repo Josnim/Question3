749b43c221657d8a18f6a2a3a8b3e87c16c73c44
Josnim <103206493+Josnim@users.noreply.github.com>
2022-11-15 00:16:11 -0500

    Add files via upload

---
+++ Prueba3/Prueba3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System; 
+using System.Linq;  
+namespace Prueba3 {   
+    class Program     
+    {         
+        public static bool palindrome(int mainint)         
+        {
+            string mainString = mainint.ToString();
+            string fh = mainString.Substring(0, mainString.Length / 2);             
+            char[] arr = mainString.ToCharArray();              
+            Array.Reverse(arr);              
+            string temp = new string(arr);             
+            string sh = temp.Substring(0, temp.Length / 2);              
+            return fh.Equals(sh);         
+        }         static void Main(string[] args)         
+        {
+            int input = int.Parse(Console.ReadLine());
+            bool result = palindrome(input);             
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }     
+    } 
+}
